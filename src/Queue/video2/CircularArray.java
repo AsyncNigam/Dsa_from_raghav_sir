220bcad0dd7521414c33b120663f86ba2053e07d
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-03-03 21:31:48 +0530

    array implementation of queue

---
+++ src/Queue/video2/CircularArray.java
@@ -0,0 +1,75 @@
+package Queue.video2;
+
+class CircularQueue {
+    int[] arr;
+    int f;
+    int r;
+    int size;
+    CircularQueue(int capacity){
+        arr=new int[capacity];
+    }
+    void add(int val){
+        if(size== arr.length){
+            System.out.println("Queue is full !");
+            return;
+        }
+        arr[r++]=val;
+        if(r== arr.length)r=0;
+        size++;
+    }
+
+    int remove(){
+        if(size==0){
+            System.out.println("Queue is Empty");
+            return -1;
+        }
+        int frontVal=arr[f];
+        f++;
+        if(f== arr.length) f=0;
+        size--;
+        return frontVal;
+    }
+
+    int peek(){
+        if(size==0){
+            System.out.println("Queue is Empty");
+            return -1;
+        }
+        return arr[f];
+    }
+
+    void display(){
+        if(size==0)return;
+        if(f>=r){
+            for(int i=f;i<arr.length;i++) System.out.print(arr[i]+" ");
+            for(int i=0;i<r;i++) System.out.print(arr[i]+" ");
+        }
+        else for(int i=f;i<r;i++) System.out.print(arr[i]+" ");
+
+        System.out.println();
+    }
+}
+
+public class CircularArray{
+    static void main() {
+        CircularQueue q=new CircularQueue(5);
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(44);
+        q.add(25);
+
+        q.display();
+        q.remove();
+        q.display();
+        q.remove();
+        q.display();
+
+        q.add(25);
+        q.add(36);
+        System.out.println(q.remove());
+        q.add(89);
+        q.display();
+        System.out.println(q.peek());
+    }
+}
