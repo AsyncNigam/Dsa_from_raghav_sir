6509eb8936d812d78a52e0fc62f4bbcfa38bc244
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-03-06 23:46:25 +0530

    1784. Check if Binary String Has at Most One Segment of Ones

---
+++ src/Apna_clg_sheet_revision/leetcode_1784.java
@@ -0,0 +1,20 @@
+package Apna_clg_sheet_revision;
+
+public class leetcode_1784 {
+
+//    Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
+
+    class Solution {
+        public boolean checkOnesSegment(String s) {
+
+
+            if(s.length()==1)return true;
+            if(s.length()==0)return false;
+            int one_count=0;
+            for(int i=0;i<s.length()-1;i++){
+                if(s.charAt(i)=='0' && s.charAt(i+1)=='1')return false;
+            }
+            return true;
+        }
+    }
+}
