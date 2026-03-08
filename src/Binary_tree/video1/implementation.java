926729a4ab5ca4698d24e8cef3d57398b7179a91
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-03-08 22:58:17 +0530

    implementations of Binary tree

---
+++ src/Binary_tree/video1/implementation.java
@@ -0,0 +1,101 @@
+package Binary_tree.video1;
+
+class Node {
+    Node left;
+    Node right;
+    int val;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+public class implementation {
+    public static void main(String[] args) {
+        //      3
+        //    /   \
+        //   -4     2
+        //  / \   / \
+        // 1  1 6   9
+
+        Node a = new Node(3);
+        Node b = new Node(-4);
+        Node c = new Node(2);
+        Node d = new Node(1);
+        Node e = new Node(1);
+        Node f = new Node(6);
+        Node g = new Node(9);
+
+        a.left = b; a.right = c;
+        b.left = d; b.right = e;
+        c.left = f; c.right = g;
+//        display(a);
+
+        System.out.println(size(a));
+        System.out.println(sum_of_nodes_Btree(a));
+        System.out.println(product_of_nodes(a));
+        System.out.println(pro_non0(a));
+        System.out.println(max_val(a));
+        System.out.println(min_val(a));
+
+    }
+
+    private static void display(Node root){
+        if(root==null)return;
+
+//        this is preOrder
+//        System.out.print(root.val+" ");
+//        display(root.left);
+//        display(root.right);
+
+//        for inOrder
+//        display(root.left);
+//        System.out.print(root.val+" ");
+//        display(root.right);
+
+//        for postOrder
+//        display(root.left);
+//        display(root.right);
+//        System.out.print(root.val+" ");
+
+    }
+
+//    size of the binary tree
+    static int size(Node root){
+        if(root==null)return 0;
+        return 1+size(root.left)+size(root.right);
+    }
+
+//    sum of values of nodes
+    static int sum_of_nodes_Btree(Node root){
+        if(root==null)return 0;
+        return root.val+sum_of_nodes_Btree(root.left)+sum_of_nodes_Btree(root.right);
+    }
+
+//    for the product of nodes value
+    static int product_of_nodes(Node root){
+        if(root==null)return 1;
+        return root.val*product_of_nodes(root.left)*product_of_nodes(root.right);
+    }
+
+//    for the product of non zero elements
+
+    static int pro_non0(Node root){
+        if(root==null)return 1;
+        if(root.val<0) return pro_non0(root.left)*pro_non0(root.right);
+        return root.val*pro_non0(root.left)*pro_non0(root.right);
+    }
+
+//    for maximum value in b tree
+    static int max_val(Node root){
+        if(root==null)return Integer.MIN_VALUE;
+        return Math.max(root.val, Math.max(max_val(root.left),max_val(root.right)));
+    }
+
+//    for minimum value in Binary tree
+    static int min_val(Node root){
+        if(root==null)return Integer.MAX_VALUE;
+        return Math.min(root.val, Math.min(min_val(root.left),min_val(root.right)));
+    }
+
+}
