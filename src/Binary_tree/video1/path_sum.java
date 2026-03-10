2cb31f2f6a135de22209cc78e505aea2dcc4b7db
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-03-11 01:07:21 +0530

    112. Path Sum

---
+++ src/Binary_tree/video1/path_sum.java
@@ -0,0 +1,20 @@
+package Binary_tree.video1;
+
+public class path_sum {
+
+
+    class Solution {
+        public boolean hasPathSum(TreeNode root, int targetSum) {
+
+            if(root==null)return false; //if we have reached leaf return false , because we have to find the path from root to leaf node if root has no leaf i.e. root is leaf return false although if equals to targetSum
+
+//          here we are not going to root ==null , we are chacking a level above
+
+            if(root.left==null && root.right==null){
+                if(targetSum==root.val)return true;
+                return false;
+            }
+            return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+        }
+    }
+}
--- src/Binary_tree/video1/symmetric_tree.java
+++ src/Binary_tree/video1/symmetric_tree.java
@@ -1,33 +1,63 @@
 package Binary_tree.video1;
 
 public class symmetric_tree {
-    class Solution {
-        public boolean isSymmetric(TreeNode root) {
 
-            TreeNode copy=copyTree(root);
-            return isSameTree(root,invertTree(copy));
-        }
-        public TreeNode copyTree(TreeNode root) {
-            if (root == null) return null;
-            return new TreeNode(root.val, copyTree(root.left), copyTree(root.right));
-        }
-        public TreeNode invertTree(TreeNode root) {
-            if(root==null)return root;
-            TreeNode temp=root.left;
-            root.left=root.right;
-            root.right=temp;
-            invertTree(root.left);
-            invertTree(root.right);
-            return root;
-        }
+//    my approach
 
-        public boolean isSameTree(TreeNode p, TreeNode q) {
-            if(p==null && q==null)return true;
-            if(p == null || q == null)return false;
-            if(p.val!=q.val)return false;
+//    class Solution {
+//        public boolean isSymmetric(TreeNode root) {
+//
+//            TreeNode copy=copyTree(root);
+//            return isSameTree(root,invertTree(copy));
+//        }
+//        public TreeNode copyTree(TreeNode root) {  //here we are creating copy because if we directly use is same function to invertion of rooot with root it automatically changes the root which will be always true so we have created copy
+//            if (root == null) return null;
+//            return new TreeNode(root.val, copyTree(root.left), copyTree(root.right));
+//        }
+//        public TreeNode invertTree(TreeNode root) {
+//            if(root==null)return root;
+//            TreeNode temp=root.left;
+//            root.left=root.right;
+//            root.right=temp;
+//            invertTree(root.left);
+//            invertTree(root.right);
+//            return root;
+//        }
+//
+//        public boolean isSameTree(TreeNode p, TreeNode q) {
+//            if(p==null && q==null)return true;
+//            if(p == null || q == null)return false;
+//            if(p.val!=q.val)return false;
+//
+//            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+//
+//        }
+//    }
 
-            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
 
-        }
+//    let's pass only the one subtree then check wth another subtree
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isSameTree(root.left,invertTree(root.right));
     }
+
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null)return root;
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)return true;
+        if(p == null || q == null)return false;
+        if(p.val!=q.val)return false;
+
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+
+    }
+}
 }
