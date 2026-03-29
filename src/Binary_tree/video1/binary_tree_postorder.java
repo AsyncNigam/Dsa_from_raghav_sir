493f038ac0bb33c7f60e009e1911f7f1b4b9a3c7
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-03-30 00:34:56 +0530

    post order iterative way

--- src/Binary_tree/video1/binary_tree_postorder.java
+++ src/Binary_tree/video1/binary_tree_postorder.java
@@ -18,4 +18,24 @@ public class binary_tree_postorder {
             ans.add(root.val);
         }
     }
+
+//    for iterative way
+
+//    class Solution {
+//    public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> ans=new ArrayList<>();
+//        if (root == null) return ans;
+//        Stack<TreeNode> st=new Stack<>();
+//        st.push(root);
+//        while(!st.isEmpty()){
+//            TreeNode temp=st.pop();
+//            ans.add(temp.val);
+//
+//            if(temp.left!=null) st.push(temp.left);
+//            if(temp.right!=null)st.push(temp.right);
+//        }
+//       Collections.reverse(ans);
+//       return ans;
+//        }
+//}
 }
