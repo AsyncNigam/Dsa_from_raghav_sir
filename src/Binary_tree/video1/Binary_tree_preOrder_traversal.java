09ba9a4a612035d226090e2c4cc193dc9c866903
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-03-30 00:18:50 +0530

    iterative approach of preorder

--- src/Binary_tree/video1/Binary_tree_preOrder_traversal.java
+++ src/Binary_tree/video1/Binary_tree_preOrder_traversal.java
@@ -2,27 +2,45 @@ package Binary_tree.video1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 
 //https://leetcode.com/problems/binary-tree-preorder-traversal/
 
 public class Binary_tree_preOrder_traversal {
-    class Solution {
-        public List<Integer> preorderTraversal(TreeNode root) {
-            List<Integer> ans=new ArrayList<>();
-            helper(root,ans);
-            return ans;
+//    class Solution {
+//        public List<Integer> preorderTraversal(TreeNode root) {
+//            List<Integer> ans=new ArrayList<>();
+//            helper(root,ans);
+//            return ans;
+//
+//        }
+//        void helper(TreeNode root,List<Integer> ans){
+//            if(root==null)return;
+//            ans.add(root.val);
+//            helper(root.left,ans);
+//
+//            helper(root.right,ans);
+//
+//        }
+//
+//
+//    }
 
+//    by iterative approach
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        if (root == null) return ans;
+        Stack<TreeNode> st=new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode temp=st.pop();
+            ans.add(temp.val);
+            if(temp.right!=null)st.push(temp.right);
+            if(temp.left!=null) st.push(temp.left);
         }
-        void helper(TreeNode root,List<Integer> ans){
-            if(root==null)return;
-            ans.add(root.val);
-            helper(root.left,ans);
-
-            helper(root.right,ans);
-
-        }
-
-
+        return ans;
     }
+}
 }
